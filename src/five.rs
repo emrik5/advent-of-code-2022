7b15ed507b8393388540864250685cff0cc26c9f
Emrik Ulwán <0512emul@skola.motala.se>
2022-12-05 19:59:11 +0100

    Initial commit

---
+++ src/five.rs
@@ -0,0 +1,6 @@
+use crate::get_puzzle_input;
+
+pub fn five() {
+    let inp = get_puzzle_input("input/five_input.txt");
+    println!("{:?}", inp);
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::error::Error;
+use std::fs;
+use std::io;
+use std::io::Split;
+
+pub mod five;
+fn main() -> Result<(), Box<dyn Error>> {
+        println!("Enter Day");
+        let mut inp = String::new();
+        io::stdin().read_line(&mut inp)?;
+        let inp: u32 = inp.trim().parse().unwrap_or(0);
+        match inp {
+            0 => println!("No or invalid input"),
+            1 => todo!(),
+            2 => todo!(),
+            3 => todo!(),
+            4 => todo!(),
+            5 => five::five(),
+            _ => unreachable!()
+        }
+    Ok(())
+}
+
+pub fn get_puzzle_input(file_path: &str) -> Vec<String> {
+    let inp = fs::read_to_string(file_path).expect("Failed to read puzzle input file");
+    let mut outp: Vec<String> = vec![];
+    let inp: Vec<&str> = inp.split('^').collect();
+    for c in inp {
+        outp.push(c.to_string());
+    }
+    outp
+    
+}
