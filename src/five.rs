2011d9564cf8a83628c623e1c115467308940d3b
emrik5 <emrikulwan@gmail.com>
2022-12-05 20:51:52 +0100

    something

--- src/five.rs
+++ src/five.rs
@@ -1,6 +1,17 @@
 use crate::get_puzzle_input;
 
 pub fn five() {
-    let inp = get_puzzle_input("input/five_input.txt");
-    println!("{:?}", inp);
+	let inp = get_puzzle_input("input/five_input.txt");
+	let crates = inp[0].to_owned();
+	let rows: [Vec<bool>; 9];
+
+	let instruct = inp[1].to_owned();
+	{
+		let mut spaces_count = 0;
+		for c in crates.chars() {
+			if c == ' ' {
+				spaces_count += 1;
+			}
+		}
+	}
 }
